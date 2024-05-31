c1b88dc26e653d1d2447d780371f187d285b5c75
YevhenKharchenko <evgenvalerych@gmail.com>
2024-05-31 22:27:39 +0300

    update

--- src/routers/students.js
+++ src/routers/students.js
@@ -8,6 +8,11 @@ import {
   patchStudentController,
 } from '../controllers/students.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createStudentSchema,
+  updateStudentSchema,
+} from '../validation/student.js';
 
 const router = Router();
 
@@ -15,12 +20,24 @@ router.get('/students', ctrlWrapper(getStudentsController));
 
 router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
 
-router.post('/students', ctrlWrapper(createStudentController));
+router.post(
+  '/students',
+  validateBody(createStudentSchema),
+  ctrlWrapper(createStudentController),
+);
 
 router.delete('/students/:studentId', ctrlWrapper(deleteStudentController));
 
-router.put('/students/:studentId', ctrlWrapper(updateStudentController));
+router.put(
+  '/students/:studentId',
+  validateBody(updateStudentSchema),
+  ctrlWrapper(updateStudentController),
+);
 
-router.patch('/students/:studentId', ctrlWrapper(patchStudentController));
+router.patch(
+  '/students/:studentId',
+  validateBody(updateStudentSchema),
+  ctrlWrapper(patchStudentController),
+);
 
 export default router;
